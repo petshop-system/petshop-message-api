9480870d158ff534f454242ed48a59fd2d92a898
MaironCosta <maironmendes@yahoo.com.br>
2024-01-15 21:19:30 -0400

    ajustando json para mensagem.

--- src/main/java/com/petshopsystem/petshopmessageapi/service/Schedule.java
+++ src/main/java/com/petshopsystem/petshopmessageapi/service/Schedule.java
@@ -1,12 +1,20 @@
 package com.petshopsystem.petshopmessageapi.service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public class Schedule {
+
+    @JsonProperty(value = "booking", required = true)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime booking;
 
+    @JsonProperty(value = "pet_id", required = true)
     private Long petID;
 
+    @JsonProperty(value = "service_employee_attention_id", required = true)
     private Long serviceEmployeeAttentionID;
 
     public Schedule() {
